b36ff4984c95765976403519bc06e31592c6f21c
Kristian <56667566+kristian821@users.noreply.github.com>
2021-04-27 21:40:42 -0700

    added Post-routes

--- routes/api/index.js
+++ routes/api/index.js
@@ -1,8 +1,9 @@
 const router = require('express').Router();
 
-const { use } = require('./user-routes');
 const userRoutes = require('./user-routes');
+const postRoutes = require('./post-routes');
 
 router.use('/users', userRoutes);
+router.use('/posts', postRoutes);
 
 module.exports = router;
