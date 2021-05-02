509474253f09a53945279d604cd232755b38f4c7
Kristian <56667566+kristian821@users.noreply.github.com>
2021-05-02 15:36:45 -0700

    Added cookies

--- controllers/api/user-routes.js
+++ controllers/api/user-routes.js
@@ -59,7 +59,13 @@ router.post('/', (req, res) => {
         email: req.body.email,
         password: req.body.password
     }).then(dbUserData => {
-        res.json(dbUserData);
+        req.session.save(() => {
+            req.session.user_id = dbUserData.id,
+            req.session.username = dbUserData.username,
+            req.session.loggedIn = true;
+
+            res.json(dbUserData);
+        })
     }).catch(e => {
         console.log(e);
         res.status(500).json(e);
@@ -76,17 +82,36 @@ router.post('/login', (req, res) => {
             res.status(400).json({ message: 'No user with that email address!' });
             return;
         }
+
         const validPassword = dbUserData.checkPassword(req.body.password);
+
         if (!validPassword) {
             res.status(400).json({ message: 'Your password was entered incorrectly.' });
             return;
         }
 
-        res.json({ user: dbUserData, message: 'You are now logged in!' });
+        req.session.save(() => {
+            req.session.user_id = dbUserData.id,
+            req.session.username = dbUserData.username,
+            req.session.loggedIn = true;
+
+            res.json({ user: dbUserData, message: 'You are now logged in!' });
+        });
+    
     }).catch(e => {
         console.log(e);
         res.status(500).json(e);
-    })
+    });
+});
+
+router.post('/logout', (req, res) => {
+    if (req.session.loggedIn) {
+        req.session.destroy(() => {
+            res.status(204).end();
+        });
+    } else {
+        res.status(404).end();
+    }
 });
 
 // PUT /api/users/1
